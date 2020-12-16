f34f912a3fbb17cf6287410ac456e2984413453b
Ethan <ethanchen229@gmail.com>
2020-12-16 09:08:33 -0500

    Added constructors

--- src/org/sla/PetsAndPeople.java
+++ src/org/sla/PetsAndPeople.java
@@ -188,41 +188,24 @@ public class PetsAndPeople {
         System.out.println("Bye!");
 
         // Ethan:
-        Owner EthanC = new Owner();
+        Owner EthanC = new Owner("Ethan Chen", 20, 3.75f, 100);
 
-        EthanC.name = "Ethan Chen";
-
-        EthanC.dogFood = 20;
         EthanC.seedsForPigeon = 100;
-        EthanC.freeTime = 3.75f;
-        EthanC.money = 100;
 
-        Dog rupertDog = new Dog();
-        rupertDog.name = "Rupert";
-        rupertDog.age = 4;
-        rupertDog.breed = "Corgi" ;
-        rupertDog.energy = 2.1f;
-        rupertDog.hunger = 0.5f;
-        rupertDog.owner = EthanC;
-        EthanC.pet1 = rupertDog;
+        allOfThePets[2] = new Dog("Rupert", "Corgi", 4, EthanC, 2.1f, 0.5f);
+        EthanC.pet1 = allOfThePets[2];
 
         EthanC.announce();
-        EthanC.walk(rupertDog, 1.1f);
-        EthanC.feed(rupertDog, 1f);
+        EthanC.walk(allOfThePets[2], 1.1f);
+        EthanC.feed(allOfThePets[2], 1f);
 
         // Pet Pigeon :D
-        Pigeon myPigeon = new Pigeon();
-        myPigeon.name = "Kevin Gertrude Bartholomew Maximus Leon III Smoo";
-        myPigeon.age = 2;
-        myPigeon.species = "Feral Pigeon";
-        myPigeon.flyTime = 0.2f;
-        myPigeon.hunger = 60f;
-        myPigeon.owner = EthanC;
-        EthanC.pet5 = myPigeon;
+        allOfThePets[3] = new Pigeon("Kevin Gertrude Bartholomew Maximus Leon III Smoo", 2, EthanC, 60f, "Feral Pigeon", 0.2f, Bella);
+        EthanC.pet5 = allOfThePets[3];
 
         EthanC.announce();
-        EthanC.fly(myPigeon, 2.3f);
-        EthanC.feed(myPigeon, 30f);
+        EthanC.fly(allOfThePets[3], 2.3f);
+        EthanC.feed(allOfThePets[3], 30f);
 
 
         // Tashon's person
